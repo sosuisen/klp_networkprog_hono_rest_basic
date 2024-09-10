ddef3f6ec3797e65d6d421b769bd2dd383ac2219
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2024-09-11 02:55:20 +0900

    fix: add types

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,15 @@ type Todo = {
 	completed: boolean;
 };
 
+type PostTodo = {
+	title: string;
+};
+
+type PutTodo = {
+	title: string;
+	completed: boolean;
+};
+
 const todos: Todo[] = [
 	{ id: 1, title: "ネーム", completed: true },
 	{ id: 2, title: "下書き", completed: false },
@@ -37,25 +46,25 @@ app.get("/todos", (c) => c.json(todos));
 // path params
 // c.get('id')はc.var.idでもよい。
 app.get("/todos/:id", validationMiddleware, (c) => {
-  const todo = todos.find((todo) => todo.id === c.get("id"));
-  return c.json(todo);
+	const todo = todos.find((todo) => todo.id === c.get("id"));
+	return c.json(todo);
 });
 
 app.post("/todos", async (c) => {
-  // バリデーションと型チェックは省略。bodyはany型。
-	const body = await c.req.json();
+	// バリデーションは課題とする。
+	const body = await c.req.json<PostTodo>();
 	let lastId = 0;
 	if (todos.length > 0) lastId = todos[todos.length - 1].id;
-  // any 型の値を他の型に代入しても、TypeScriptは型をチェックせずにスルー。
+
 	const newTodo = { ...body, id: lastId + 1, completed: false };
 	todos.push(newTodo);
 	return c.json(newTodo, 201);
 });
 
 app.put("/todos/:id", validationMiddleware, async (c) => {
-	const body = await c.req.json();
+	const body = await c.req.json<PutTodo>();
 	const index = todos.findIndex((todo) => todo.id === c.get("id"));
-  const newTodo = { ...body, id: todos[index].id };
+	const newTodo = { ...body, id: todos[index].id };
 	todos[index] = newTodo;
 	return c.json(todos[index]);
 });
